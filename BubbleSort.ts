fcfd78ba90732a776e33ffac27d7e84b8642c7b4
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-11-24 03:03:55 +0530

    Bubble Sort Algorithm

---
+++ BubbleSort.ts
@@ -0,0 +1,16 @@
+function bubbleSort(arr: number[]): number[] {
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 3, 8, 2, 1, 4]));
